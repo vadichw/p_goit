d24b93ed0dfb0aad956a7193d99c0cebb9f9fa26
vadichw <vadik56780098@gmail.com>
2024-04-03 23:05:12 +0300

    add

---
+++ python_web/modul_9_web/simple_scrapy/test_spyder/test_spyder/spiders/authors.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+# scrapy crawl authors.- запускаем в консоле
+class AuthorsSpider(scrapy.Spider):
+    name = 'authors'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        for quote in response.xpath("/html//div[@class='quote']"):
+            yield {
+                "keywords": quote.xpath("div[@class='tags']/a/text()").extract(),
+                "author": quote.xpath("span/small/text()").extract(),
+                "quote": quote.xpath("span[@class='text']/text()").get()
+            }
+
+
+
+
