8f3131ff834ff3dc0bd0deab48e488273e43bf09
vadichw <vadik56780098@gmail.com>
2024-04-06 14:32:06 +0300

    add

--- python_web/modul_9_web/hw_9/base.py
+++ python_web/modul_9_web/hw_9/base.py
@@ -1,21 +1,17 @@
 import pymongo
 from main import quotes, authors
 
-# Підключення до MongoDB
 client = pymongo.MongoClient("mongodb+srv://vadich_w:I3wv0Kc9vuy0T96p@cluster0.n7wujvu.mongodb.net/?retryWrites=true"
                              "&w=majority&appName=Cluster0")
 db = client["hw9"]
 quotes_collection = db["quotes"]
 authors_collection = db["authors"]
 
-# Збереження цитат у колекцію
 for quote in quotes:
     quotes_collection.insert_one(quote)
 
-# Збереження авторів у колекцію
-
 for author_name in authors:
-    author_doc = {"name": author_name}  # Створюємо словник з іменем автора
+    author_doc = {"name": author_name}
     authors_collection.insert_one(author_doc)
 
-print("Дані збережено у базу даних MongoDB.")
+print("Data is saved to database")
--- python_web/modul_9_web/hw_9/main.py
+++ python_web/modul_9_web/hw_9/main.py
@@ -3,34 +3,37 @@ from bs4 import BeautifulSoup
 import json
 
 
-# Функція для отримання даних про цитати
 def scrape_quotes(url):
     quotes = []
-    authors = set()
+    authors = []
     while url:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
+
         for quote in soup.select('div.quote'):
+
             text = quote.select_one('span.text').get_text()
             author = quote.select_one('small.author').get_text()
-            authors.add(author)
+            authors.append(author)
             tags = [tag.get_text() for tag in quote.select('a.tag')]
             quotes.append({'text': text, 'author': author, 'tags': tags})
+
         next_page = soup.select_one('li.next > a')
         url = next_page['href'] if next_page else None
         if url and not url.startswith('http'):
-            url = f'http://quotes.toscrape.com{url}'  # Повний URL для наступної сторінки
+            url = f'http://quotes.toscrape.com{url}'
+
     return quotes, list(authors)
 
 
-# Функція для збереження даних у JSON-файл
 def save_to_json(data, filename):
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 
-# Отримання цитат та авторів
 quotes, authors = scrape_quotes("http://quotes.toscrape.com/")
-# Збереження цитат та авторів у JSON-файли
+
 save_to_json(quotes, 'quotes.json')
 save_to_json(authors, 'authors.json')
+
+print("The files were created and saved")
