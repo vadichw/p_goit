2878132c47f4eef319bde83522af22a3525b45a6
vadichw <vadik56780098@gmail.com>
2024-04-13 12:09:40 +0300

    add

--- python_web/modul_10_web/second/notes/noteapp/urls.py
+++ python_web/modul_10_web/second/notes/noteapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('tag/', views.tag, name='tag'),
     path('note/', views.note, name='note'),
     path('detail/<int:note_id>', views.detail, name='detail'),
+    path('done/<int:note_id>', views.set_done, name='set_done'),
+    path('delete/<int:note_id>', views.delete_note, name='delete'),
 ]
 
 
--- python_web/modul_10_web/second/notes/noteapp/views.py
+++ python_web/modul_10_web/second/notes/noteapp/views.py
@@ -6,7 +6,20 @@ from .models import Tag, Note
 
 # Create your views here.
 def main(request):
-    return render(request, 'noteapp/index.html')
+    notes = Note.objects.all()
+    return render(request, 'noteapp/index.html', {"notes": notes})
+
+
+# Перша функція set_done буде відповідати, що нотатка змінює статус на виконана:
+def set_done(request, note_id):
+    Note.objects.filter(pk=note_id).update(done=True)
+    return redirect(to='noteapp:main')
+
+
+# Друга функція delete_note відповідає за видалення нотатки.
+def delete_note(request, note_id):
+    Note.objects.get(pk=note_id).delete()
+    return redirect(to='noteapp:main')
 
 
 def tag(request):
