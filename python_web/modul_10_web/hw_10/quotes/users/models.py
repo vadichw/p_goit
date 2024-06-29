6adbedf3b8c0ff32b89740fba68fcbfdbd004dc7
vadichw <vadik56780098@gmail.com>
2024-06-29 18:38:26 +0300

    add

--- python_web/modul_10_web/hw_10/quotes/users/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
